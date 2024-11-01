e3f8aeb9a69e3dc0aa5fa1819455f5fed7178ecb
Malika <elabiadmalika64@gmail.com>
2024-11-01 09:54:24 +0100

    Ajout de partie formulaire #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./projet/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#155e75',
+        borderColor: '#5f5d5d',
+        bgcolor: '#F3F3F3',
+
+      },
+    },
   },
   plugins: [],
 }
