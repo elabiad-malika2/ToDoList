d405e447a78a04cffef8bec125dee2cc3e9cfe32
Malika <elabiadmalika64@gmail.com>
2024-11-02 16:26:58 +0100

    Ajout de partie de changement de couleur avec priorite #3

--- projet/main.js
+++ projet/main.js
@@ -53,9 +53,13 @@ function afficherTaches(){
             <div class="absolute top-0 left-0 w-full h-2 ${tache.priorite === '1' ? 'bg-red-500' : tache.priorite === '2' ? 'bg-black' : 'bg-green-500'} rounded-t-3xl"></div>
             <div class="flex flex-col px-4 h-full justify-center items-center relative">
                 <div class="absolute top-4 right-2" >
-                    <svg onclick="showEdit()" xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" viewBox="0 0 24 24" class="fill-current text-gray-600">
+                    <svg onclick="showEdit(${index})" xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" viewBox="0 0 24 24" class="fill-current text-gray-600">
                         <path d="m14.06 9.02l.92.92L5.92 19H5v-.92zM17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83l3.75 3.75l1.83-1.83a.996.996 0 0 0 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29m-3.6 3.19L3 17.25V21h3.75L17.81 9.94z"/>
                     </svg>
+                    </div>
+                <div class="absolute top-4 right-8" >
+
+                    <svg onclick="deleteTache(${index})" xmlns="http://www.w3.org/2000/svg" width="1.2rem" height="1.2rem" viewBox="0 0 24 24"><path fill="#990000" d="M7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21zm2-4h2V8H9zm4 0h2V8h-2z"/></svg>
                 </div>
                 <div class="font-semibold text-black mt-4">${tache.title}</div>
                 <div class="font-semibold text-gray-800 mt-4">${tache.description}</div>
@@ -74,8 +78,34 @@ function afficherTaches(){
     
 }
 afficherTaches();
-function showEdit(){
+function showEdit(index){
+    const tache = JSON.parse(localStorage.getItem("taches")) || [];
     modeleEdit.style.display = "flex";
+    titreEdit.value = tache[index].title;
+    prioriteEdit.value = tache[index].priorite;
+    statusEdit.value=tache[index].status;
+    dateEdit.value=tache[index].date;
+    descriptionEdit.value=tache[index].description;
 
+    
+    
+    modeleEdit.addEventListener("submit",function(){
+        let tacheEdit ={
+            title:titreEdit.value,
+            description:descriptionEdit.value,
+            date:dateEdit.value,
+            status:statusEdit.value,
+            priorite:prioriteEdit.value
+        }
+        
+        tache[index]=tacheEdit;
+        localStorage.setItem("taches",JSON.stringify(tache));
+    })
+
+    afficherTaches();
+
+}
+function deleteTache(index){
+    
 
 }
